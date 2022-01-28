f06814d7de8eb74bf5a8963932716ce51ba32d27
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-01-28 12:24:24 +0100

    Add room to embed in Iframe

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const app = express();
 // app.use(express.static(path.join(__dirname, "public")));
@@ -14,20 +16,35 @@ app.use(express.json({ type: "application/json" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 
-// app.use("/", indexRouter);
+// CORS POLICY
+const corsOptions = {
+  origin: process.env.ORIGIN,
+  optionsSuccessStatus: 200,
+  methods: "OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // include before other routes
+
+
+app.use("/", indexRouter);
+
+var router = express.Router();
 
 const API_KEY = process.env.API_KEY;
-console.log(API_KEY);
 
 const data = {
-  endDate: "2022-01-26T08:50:00.000Z",
+  endDate: "2022-02-28T08:50:00.000Z",
   fields: ["hostRoomUrl"],
 };
 
@@ -38,16 +55,24 @@ function getResponse() {
             Authorization: `Bearer ${API_KEY}`,
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data)
     });
 }
 
-getResponse().then(async res => {
-    console.log("Status code:", res.status);
-    const data = await res.json();
-    console.log("Room URL:", data.roomUrl);
-    console.log("Host room URL:", data.hostRoomUrl);
-});
+const createMeeting =((req, res) => {
+  console.log('I am here');
+  getResponse().then(async result => {
+    console.log(res);
+    console.log("Status code:", result.status);
+    const data = await result.json();
+    res.json({ status: 200, msg: 'Meeting URL you requested for ', data});
+    console.log('Here is the data send to FE', data);
+  });
+})
+
+/* GET home page. */
+app.use('/create-meeting', router.get('/', createMeeting));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
